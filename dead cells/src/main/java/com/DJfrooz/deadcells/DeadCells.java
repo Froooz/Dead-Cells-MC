af873d47f0802da3a820630f0da1849eb56fcec8
Frooz <chicken.games124@gmail.com>
2021-12-29 10:20:20 +1300

    Add files via upload

---
+++ dead cells/src/main/java/com/DJfrooz/deadcells/DeadCells.java
@@ -0,0 +1,13 @@
+package com.DJfrooz.deadcells;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DeadCells.MODID)
+public class DeadCells {
+	public static final String MODID = "deadcells";
+	
+	public DeadCells() {
+		
+	}
+
+}
