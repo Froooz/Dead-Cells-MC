ff7041a1b70bc9c4f49cea9520af14f34341697d
Frooz <chicken.games124@gmail.com>
2021-12-29 14:34:41 +1300

    item registries

---
+++ dead cells/src/main/java/com/DJfrooz/deadcells/core/init/DeadCellsItems.java
@@ -0,0 +1,18 @@
+package com.DJfrooz.deadcells.core.init;
+
+import com.DJfrooz.deadcells.DeadCells;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public final class DeadCellsItems {
+	private DeadCellsItems() {}
+	
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DeadCells.MODID);
+	
+	public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+			() -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
+}
